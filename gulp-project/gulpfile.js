84c587a83706037fcbf681a6d994226dad446155
SaintBooth <ya.booth@yandex.ru>
2025-06-25 19:31:27 +0500

    header done

---
+++ gulp-project/gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat-css');
+const plumber = require('gulp-plumber');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+function html() {
+  return gulp.src('src/**/*.html')
+        .pipe(plumber())
+                .pipe(gulp.dest('dist/'))
+}
+
+exports.html = html;
